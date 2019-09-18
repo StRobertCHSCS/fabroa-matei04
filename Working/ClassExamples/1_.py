9cbe70fd0649a6cc83c22048e67a752088e6226f
matei04 <matei.gornic22@ycdsbk12.ca>
2019-09-18 08:56:03 -0400

    print examples

---
+++ Working/ClassExamples/1_.py
@@ -0,0 +1,11 @@
+print("This is a statement")
+print ("What's wrong with this statement")
+print ("How about ths one")
+print(5)
+print(3.14)
+print("here are the ages of my three children:", 12, 7, 4)
+greeting = "Hello world!"
+print (greeting)
+colour = "black"
+print("His shoes are", colour)
+print("His pants are", colour)
