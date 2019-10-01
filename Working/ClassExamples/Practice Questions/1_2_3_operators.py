06a1f51028b773ae8e8d250d0feab4b73170c044
matei04 <matei.gornic22@ycdsbk12.ca>
2019-10-01 09:09:40 -0400

    new live hack files

---
+++ Working/ClassExamples/Practice Questions/1_2_3_operators.py
@@ -0,0 +1,3 @@
+name = "Babatunde Olaganuiamfiemi"
+age = 73
+print ("Yo! I go by", str(name) + "and I am", str(age))
---
+++ Working/ClassExamples/Practice Questions/1_5 practice and examples.py
@@ -0,0 +1,8 @@
+
+print("1 + 1 = "  + str(2))
+
+print ("I want to print a double quote \" for some reason.")
+print ("this is how to print a double quote using a back slash")
+print("Save all your files to the C:\\ drive")
+print("first column\tsecond column\tthird column")
+print("first line\nsecond line\nthird line")
