a12f67960670513d1b8495dcbb4d47c415b01fb8
matei04 <matei.gornic22@ycdsbk12.ca>
2019-11-18 09:47:47 -0500

    test

---
+++ Working/LiveHacks/livehack2_problem.py
@@ -0,0 +1,47 @@
+
+#Filename:       livehack2_problem.py
+
+
+#Description:    Determine wether a triangle is isoceles, equilateral or scalene using angles
+
+
+#Author:         Gornic.M
+
+
+#Created On:     11/18/2019
+
+# get angle values
+
+
+verticy1 = int(input("Enter the degree of angle 1: "))
+
+
+verticy2 = int(input("Enter the degree of angle 2: "))
+
+
+verticy3 = int(input("Enter the degree of angle 3: "))
+
+# compute sum of angles
+
+if (verticy1+verticy2+verticy3 = 180)
+
+print("These angles make up a triangle")
+
+else:
+
+    print("error, these angles do not make up a triangle")
+
+# determine what type of triangle
+
+if (verticy1=verticy2) or (verticy=verticy3) or (verticy2=verticy1) or (vercity2=vercity3) or (vercity3=vercity1) or (vercity3=vercity2)
+
+print("It is an isoceles triangle")
+
+elif (verticy1=verticy2=verticy3)
+
+print("It is an equilateral triangle")
+
+else: 
+
+    print("It is a scalene triangle")
+
